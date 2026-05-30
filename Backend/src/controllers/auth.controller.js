99a000cfc3add5f19e95b635639762e24c7a74c2
Delta Student <harshadsolanke7789@gmail.com>
2026-05-31 00:07:22 +0530

    backend file update

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -102,8 +102,23 @@ async function logoutUserController(req, res){
 
 }
 
+async function getMeController(req, res){
+
+    const user = await userModel.findById(req.user.id)
+
+    res.status(200).json({
+        message: "user details fetched successfully",   
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
+
 module.exports = {
     registerUserController,
     loginUserController,
     logoutUserController,
+    getMeController,
 }
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authController = require('../controllers/auth.controller')
+const authMiddleware = require("../middlewares/auth.middleware")
 
 const authRouter = express.Router()
 
@@ -8,5 +9,9 @@ const authRouter = express.Router()
  authRouter.post("/login", authController.loginUserController)
 
  authRouter.get("/logout", authController.logoutUserController)
+
+ authRouter.get("/get-me",authMiddleware.authUser,authController.getMeController)
+
+
 
 module.exports = authRouter;
